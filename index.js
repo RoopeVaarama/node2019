cfed6657fc50cb642a3de6fe5f9d7990c1508331
Roope Vaarama <roope.vaarama@gmail.com>
2019-11-18 11:40:21 +0200

    commit

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 'use strict';
 
+require("dotenv").config();
+
 const express = require('express');
+const connection = require(".(model/db.js");
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.get("/animals", async (req, res) => {
+   try {
+       const [results, field] = await connection.query(
+           "SELECT * FROM animals");
+       console.log(results);
+       console.log(fields);
+       res.json(results);
+   } catch (e) {
+       console.log(e);
+       res.send("db error :(");
+   }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello from my Node server');
